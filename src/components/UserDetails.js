e537ac9f1a49681879ba9c1bd82eac54b274634e
niranjanv849 <niranjanv849@gmail.com>
2022-05-13 15:22:20 +0530

    useParams() Hook 🚀

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function UserDetails() {
-  return <div>UserDetails</div>;
+  const params = useParams();
+  const userId = params.userId;
+  return <div>UserDetails about {userId}</div>;
 }
 
 export default UserDetails;
