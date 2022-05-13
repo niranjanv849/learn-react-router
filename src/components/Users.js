3c9855594b170463e7643388ffe6d126f38c8e40
niranjanv849 <niranjanv849@gmail.com>
2022-05-13 15:29:44 +0530

    useSearchParams() Hook✅🔥

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+
 function Users() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active";
   return (
     <div>
       <h2>User 1</h2>
       <h2>User 2</h2>
       <h2>User 3</h2>
+      <br />
+      <br />
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Active Users
+      </button>
+      <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all default users</h2>
+      )}
       <Outlet />
     </div>
   );
