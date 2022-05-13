39fb08d6b8e7b4b0707c704aa628adf754b4775e
niranjanv849 <niranjanv849@gmail.com>
2022-05-13 14:07:29 +0530

    Navbar Active using CSS🎨

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
-      Hello there
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;
