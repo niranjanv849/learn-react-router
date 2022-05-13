292a38ae759daaa2dd0500c89f64e4a847bee21c
niranjanv849 <niranjanv849@gmail.com>
2022-05-13 14:51:54 +0530

    Dynamic Route config 🌋✅

--- src/App.js
+++ src/App.js
@@ -25,9 +25,10 @@ function App() {
           <Route path="feature" element={<Feature />} />
           <Route path="new" element={<New />} />
         </Route>
-        <Route path="users" element={<Users />} />
-        <Route path="users/:userId" element={<UserDetails />} />
-        <Route path="users/admin" element={<Admin />} />
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 function Users() {
   return (
     <div>
       <h2>User 1</h2>
       <h2>User 2</h2>
       <h2>User 3</h2>
+      <Outlet />
     </div>
   );
 }
